86a97c354205090f429a50b91310551912cb48c2
ewhalgand <halgandewen44@gmail.com>
2025-02-28 02:44:39 +0100

    feat: added Sanity in backend

---
+++ src/lib/sanityClient.ts
@@ -0,0 +1,22 @@
+import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+const client = createClient({
+  projectId: 'dq2dnyrv',
+  dataset: 'production',
+  apiVersion: '2025-02-27',
+  useCdn: false, // Désactivez le CDN pour utiliser le token
+  token: import.meta.env.SANITY_TOKEN, // Utilisez une variable d'environnement
+});
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: string) {
+  return builder.image(source);
+}
+
+export async function getProjets() {
+  const query = '*[_type == "project"]';
+  const projets = await client.fetch(query);
+  return projets;
+}
